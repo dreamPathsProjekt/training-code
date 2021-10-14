dd71324f7a85a56e7df76d7897a8b3047d9e23f1
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2021-10-14 16:10:11 +0300

    Update DIP notes anf fix LF issues.

--- python_design_patterns/code/udemy/SOLID/dip.py
+++ python_design_patterns/code/udemy/SOLID/dip.py
@@ -19,30 +19,34 @@ class RelationshipBrowser:
 
 
 class Relationships(RelationshipBrowser):  # low-level
+    # List of tuples can be simplified in Python, without the Relationship being a class.
+    # Great candidate to refactor using namedtuple.
+    # Go equivalent would be slice of structs.
     relations = []
 
     def add_parent_and_child(self, parent, child):
         self.relations.append((parent, Relationship.PARENT, child))
         self.relations.append((child, Relationship.PARENT, parent))
-            
+
     def find_all_children_of(self, name):
         for r in self.relations:
             if r[0].name == name and r[1] == Relationship.PARENT:
                 yield r[2].name
 
-
+# We have added typing annotations to indicate dependency inversion better.
+# Otherwise duck-typing can work with swapping low-level modules.
 class Research:
     # dependency on a low-level module directly
     # bad because strongly dependent on e.g. storage type
 
-    # def __init__(self, relationships):
+    # def __init__(self, relationships: Relationships):
     #     # high-level: find all of john's children
     #     relations = relationships.relations
     #     for r in relations:
     #         if r[0].name == 'John' and r[1] == Relationship.PARENT:
     #             print(f'John has a child called {r[2].name}.')
 
-    def __init__(self, browser):
+    def __init__(self, browser: RelationshipBrowser):
         for p in browser.find_all_children_of("John"):
             print(f'John has a child called {p}')
 
